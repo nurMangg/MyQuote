1337d0cdbd5e2ed5d2cdeb8dc38498544c8a757d
nurmangg <rohmanuyeoke@gmail.com>
2024-03-21 21:44:13 +0700

    Update: JSONArray (data banyak)

--- app/src/main/java/com/rohman/myquote/MainActivity.kt
+++ app/src/main/java/com/rohman/myquote/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rohman.myquote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -25,6 +26,10 @@ class MainActivity : AppCompatActivity() {
 
 
         getRandomQuote()
+
+        binding.btnAllQuotes.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ListQuotesActivity::class.java))
+        }
 
     }
 
